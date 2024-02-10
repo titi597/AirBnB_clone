422003c1b451043e91d8b737764ca63140124c91
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-10 10:06:14 -0800

    updates

--- console.py
+++ console.py
@@ -2,68 +2,12 @@
 """the console."""
 import cmd
 import json
-import re
-from shlex import split
-from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-
-
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
-    __classes = {
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Place",
-        "Amenity",
-        "Review"
-    }
-
-    def default(self, arg):
-        """Default behavior for cmd module when input is invalid"""
-        argdict = {
-            "all": self.do_all,
-            "show": self.do_show,
-            "destroy": self.do_destroy,
-            "count": self.do_count,
-            "update": self.do_update
-        }
-        match = re.search(r"\.", arg)
-        if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
-            match = re.search(r"\((.*?)\)", argl[1])
-            if match is not None:
-                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
-                    return argdict[command[0]](call)
-        print("*** Unknown syntax: {}".format(arg))
-        return False
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -80,113 +24,123 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Create a new instance of BaseModel, save it, and print the id"""
-        args = parse(arg)
-        if len(args) == 0:
+        if not arg:
             print("** class name missing **")
-        elif args[0] not in HBNBCommand.__classes:
+            return
+        classes = {"BaseModel": BaseModel, "User": User}
+        args = arg.split()
+        class_name = args[0]
+        if class_name not in classes:
             print("** class doesn't exist **")
-        else:
-            print(eval(args[0])().id)
-            storage.save()
+            return
+        new_instance = classes[class_name]()
+        new_instance.save()
+        print(new_instance.id)
 
     def do_show(self, arg):
         """Prints the string representation of an instance"""
-        args = parse(arg)
-        objdict = storage.all()
-        if len(args) == 0:
+        args = arg.split()
+        if not arg:
             print("** class name missing **")
-        elif args[0] not in HBNBCommand.__classes:
+            return
+        elif args[0] not in ["BaseModel"]:
             print("** class doesn't exist **")
+            return
         elif len(args) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(args[0], args[1]) not in objdict:
+            return
+        key = "{}.{}".format(args[0], args[1])
+        try:
+            with open("file.json", 'r') as file:
+                objs = json.load(file)
+        except FileNotFoundError:
+            print("** no instance found **")
+            return
+        if key not in objs.keys():
             print("** no instance found **")
         else:
-            print(objdict["{}.{}".format(args[0], args[1])])
+            print(objs[key])
 
     def do_destroy(self, arg):
         """Deletes an instance"""
-        args = parse(arg)
-        objdict = storage.all()
-        if len(args) == 0:
+        args = arg.split()
+        if not arg:
             print("** class name missing **")
-        elif args[0] not in HBNBCommand.__classes:
+            return
+        elif args[0] not in ["BaseModel"]:
             print("** class doesn't exist **")
+            return
         elif len(args) == 1:
             print("** instance id missing **")
-        elif "{}.{}".format(args[0], args[1]) not in objdict.keys():
+            return
+        key = "{}.{}".format(args[0], args[1])
+        try:
+            with open("file.json", 'r') as file:
+                objs = json.load(file)
+        except FileNotFoundError:
+            print("** no instance found **")
+            return
+        if key not in objs.keys():
             print("** no instance found **")
         else:
-            del objdict["{}.{}".format(args[0], args[1])]
-            storage.save()
+            del objs[key]
+            with open("file.json", 'w') as file:
+                json.dump(objs, file)
 
     def do_all(self, arg):
         """Prints all string representation of all instances"""
-        args = parse(arg)
-        if len(args) > 0 and args[0] not in HBNBCommand.__classes:
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        elif args[0] not in ["BaseModel"]:
             print("** class doesn't exist **")
-        else:
-            objl = []
-            for obj in storage.all().values():
-                if len(args) > 0 and args[0] == obj.__class__.__name__:
-                    objl.append(obj.__str__())
-                elif len(args) == 0:
-                    objl.append(obj.__str__())
-            print(objl)
-
-    def do_count(self, arg):
-        """Retrieving the number of instances of a given class."""
-        args = parse(arg)
-        count = 0
-        for obj in storage.all().values():
-            if args[0] == obj.__class__.__name__:
-                count += 1
-        print(count)
+            return
+        try:
+            with open("file.json", 'r') as file:
+                objs = json.load(file)
+        except FileNotFoundError:
+            print("[]")
+            return
+        objs_list = [str(obj) for key, obj in objs.items() if args[0] in key]
+        print(objs_list)
 
     def do_update(self, arg):
         """Updates an instance"""
-        args = parse(arg)
-        objdict = storage.all()
-
-        if len(args) == 0:
+        args = arg.split()
+        if not arg:
             print("** class name missing **")
-            return False
-        if args[0] not in HBNBCommand.__classes:
+            return
+        elif args[0] not in ["BaseModel"]:
             print("** class doesn't exist **")
-            return False
-        if len(args) == 1:
+            return
+        elif len(args) == 1:
             print("** instance id missing **")
-            return False
-        if "{}.{}".format(args[0], args[1]) not in objdict.keys():
-            print("** no instance found **")
-            return False
-        if len(args) == 2:
+            return
+        elif len(args) == 2:
             print("** attribute name missing **")
-            return False
-        if len(args) == 3:
-            try:
-                type(eval(args[2])) != dict
-            except NameError:
-                print("** value missing **")
-                return False
-
-        if len(args) == 4:
-            obj = objdict["{}.{}".format(args[0], args[1])]
-            if args[2] in obj.__class__.__dict__.keys():
-                valtype = type(obj.__class__.__dict__[args[2]])
-                obj.__dict__[args[2]] = valtype(args[3])
-            else:
-                obj.__dict__[args[2]] = args[3]
-        elif type(eval(args[2])) == dict:
-            obj = objdict["{}.{}".format(args[0], args[1])]
-            for k, v in eval(args[2]).items():
-                if (k in obj.__class__.__dict__.keys() and
-                        type(obj.__class__.__dict__[k]) in {str, int, float}):
-                    valtype = type(obj.__class__.__dict__[k])
-                    obj.__dict__[k] = valtype(v)
-                else:
-                    obj.__dict__[k] = v
-        storage.save()
+            return
+        elif len(args) == 3:
+            print("** value missing **")
+            return
+        key = "{}.{}".format(args[0], args[1])
+        try:
+            with open("file.json", 'r') as file:
+                objs = json.load(file)
+        except FileNotFoundError:
+            print("** no instance found **")
+            return
+        if key not in objs.keys():
+            print("** no instance found **")
+            return
+        if args[2] in ["id", "created_at", "updated_at"]:
+            print("** attribute can't be updated **")
+            return
+        obj = objs[key]
+        setattr(obj, args[2], args[3])
+        objs[key] = obj
+        with open("file.json", 'w') as file:
+            json.dump(objs, file)
 
     def emptyline(self):
         """Do nothing on empty input line"""
