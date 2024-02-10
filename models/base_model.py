dfc3218062b40a2659c7c4727bbc41ae0086a280
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-10 11:45:02 -0800

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+"""defining base model."""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
+    """representing base models."""
+
     def __init__(self):
+        """Initializing base models."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """Return the str representation of the BaseModel instance."""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Updating updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance."""
+
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
