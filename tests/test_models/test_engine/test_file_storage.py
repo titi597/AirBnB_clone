13d8eaf8a79b7a05e1c46e33bfa22f628dd34fbb
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-11 13:08:12 -0800

    8. First User

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 import unittest
 import os
 from models.base_model import BaseModel
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
@@ -18,7 +19,7 @@ class TestFileStorage(unittest.TestCase):
         if os.path.exists(self.storage._FileStorage__file_path):
             os.remove(self.storage._FileStorage__file_path)
 
-    def test_save_and_reload(self):
+    def test_save_and_reload_base_model(self):
         # Create a BaseModel instance and save it
         obj = BaseModel()
         obj.save()
@@ -26,6 +27,19 @@ class TestFileStorage(unittest.TestCase):
         # Reload the storage and check if the object is in the storage
         self.storage.reload()
         self.assertIn(f"BaseModel.{obj.id}", self.storage.all())
+
+    def test_save_and_reload_user(self):
+        # Create a User instance and save it
+        user = User()
+        user.email = "test@example.com"
+        user.password = "password"
+        user.first_name = "John"
+        user.last_name = "Doe"
+        user.save()
+
+        # Reload the storage and check if the object is in the storage
+        self.storage.reload()
+        self.assertIn(f"User.{user.id}", self.storage.all())
 
     def test_new_method(self):
         # Create a BaseModel instance and add it to the storage
@@ -39,9 +53,9 @@ class TestFileStorage(unittest.TestCase):
         # Create a BaseModel instance and add it to the storage
         obj = BaseModel()
         self.storage.new(obj)
-
-        # Save the storage and check if the JSON file is created
         self.storage.save()
+
+        # Check if the JSON file is created
         self.assertTrue(os.path.exists(self.storage._FileStorage__file_path))
 
     def test_reload_method_without_file(self):
