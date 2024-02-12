8c37371209f9cd0bb244a859b7f35c9d66c125bd
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-12 01:49:42 -0800

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import json
 import cmd
-from models.base_model import BaseModel
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
