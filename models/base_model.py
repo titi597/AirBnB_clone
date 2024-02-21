d5a2ca58257ceedd8225c911a9979fb384719117
titi597 <139129034+titi597@users.noreply.github.com>
2024-02-21 01:43:18 -0800

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""defining BaseModel."""
+"""defining basemodel."""
 import uuid
 from datetime import datetime
 
---
+++ tests/test_base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def setUp(self):
+        self.base_model = BaseModel()
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(self.base_model, 'id'))
+        self.assertTrue(hasattr(self.base_model, 'created_at'))
+        self.assertTrue(hasattr(self.base_model, 'updated_at'))
+
+    def test_id_unique(self):
+        other_model = BaseModel()
+        self.assertNotEqual(self.base_model.id, other_model.id)
+
+    def test_str_method(self):
+        expected_str = "[BaseModel] ({}) {}".format(
+            self.base_model.id, self.base_model.__dict__)
+        self.assertEqual(str(self.base_model), expected_str)
+
+    def test_save_method(self):
+        old_updated_at = self.base_model.updated_at
+        self.base_model.save()
+        self.assertNotEqual(old_updated_at, self.base_model.updated_at)
+
+    def test_to_dict_method(self):
+        obj_dict = self.base_model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(obj_dict['created_at'], str)
+        self.assertIsInstance(obj_dict['updated_at'], str)
+
+
+if __name__ == '__main__':
+    unittest.main()
